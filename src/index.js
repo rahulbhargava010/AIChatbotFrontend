3e355c6fcc858e5d31bfdd2aa59a0ed5c76e5554
Mansi <mansi@propstory.com>
2025-03-24 17:05:44 +0530

    Removed strict mode from index.js file

--- src/index.js
+++ src/index.js
@@ -9,11 +9,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <HelmetProvider>
-            <App />
-        </HelmetProvider>
-    </React.StrictMode>
+    <HelmetProvider>
+        <App />
+    </HelmetProvider>
+
+    // <React.StrictMode>
+    //     <HelmetProvider>
+    //         <App />
+    //     </HelmetProvider>
+    // </React.StrictMode>
 );
 
 // ReactDOM.render(
